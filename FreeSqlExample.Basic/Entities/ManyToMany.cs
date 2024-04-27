922d42f091584d7e90d32039fb48e9ae715bf51e
锋 杨 <yangf85@163.com>
2024-04-27 16:57:00 +0800

    修改

---
+++ FreeSqlExample.Basic/Entities/ManyToMany.cs
@@ -0,0 +1,42 @@
+﻿using FreeSql.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeSqlExample.Basic.Entities
+{
+    public class DeliveryReport
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Navigate(ManyToMany = typeof(DeliveryReportPart))]
+        public virtual ICollection<Part> Parts { get; set; }
+    }
+
+    public class Part
+    {
+        public int Id { get; set; }
+
+        public string PartName { get; set; }
+
+        public int Piece { get; set; }
+
+        [Navigate(ManyToMany = typeof(DeliveryReportPart))]
+        public virtual ICollection<DeliveryReport> DeliveryReports { get; set; }
+    }
+
+    public class DeliveryReportPart
+    {
+        public int DeliveryReportId { get; set; }
+
+        public virtual DeliveryReport DeliveryReport { get; set; }
+
+        public int PartId { get; set; }
+
+        public virtual Part Part { get; set; }
+    }
+}
---
+++ FreeSqlExample.Lesson13_ManyToMany/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FreeSqlExample.Basic;
+using FreeSqlExample.Basic.Entities;
+
+Console.WriteLine("Hello, World!");
+
+BasicOrm.ClearTableData<Part>();
+
+var parts = new List<Part>
+{
+    new Part { PartName = "Part1",Piece=35 },
+    new Part { PartName = "Part2",Piece=27 }
+};
+
+BasicOrm.Orm.Insert(parts);
+
+// 创建一个新的 DeliveryReport 实例
+var deliveryReport = new DeliveryReport
+{
+    Name = "New Report"
+};
+
+BasicOrm.Orm.Insert(deliveryReport);
